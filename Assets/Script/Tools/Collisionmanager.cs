25aed9b4ecbb0f21a2c8477bc5489cba8e4f1fb7
Benjamin ROECKER <benjamin.roecker@efrei.net>
2022-11-23 08:44:28 +0100

    mouvement cube

--- Assets/Script/Tools/Collisionmanager.cs
+++ Assets/Script/Tools/Collisionmanager.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Collisionmanager : MonoBehaviour
 {
     [SerializeField] private UIManager uIManager;
+    [SerializeField] private int CubeForce;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(Vector3.forward * Input.GetAxis("Vertical") * CubeForce);
+        rb.AddForce(Vector3.right * Input.GetAxis("Horizontal") * CubeForce);
     }
 
     private void OnCollisionEnter(Collision collision)
