84351a6b8448d992d0ec4868d8ca82c7db324a04
Benjamin ROECKER <benjamin.roecker@efrei.net>
2022-12-06 08:36:35 +0100

    Ajout scriptableObjects

---
+++ Assets/ScriptableObject/CarteSO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Carte", menuName ="EFREI/ScriptableObjects/Cartes")]
+public class CarteSO : ScriptableObject
+{
+    public int Attack;
+    public int Defense;
+    public CarteType carteType;
+    public string Description;
+    public Sprite carteImage;
+}
+
+public enum CarteType{Commun,Rare, Legendaire}
